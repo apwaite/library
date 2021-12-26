d6f66d4d79c73ccebb82b9259cc0f20d44b3f943
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-12-26 23:59:43 +0000

    Cleaned up code

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   getData();
 });
 
 // Add new book button functionality
-const addBookForm = document.querySelector(".add-book");
+const addBookForm = document.querySelector('.add-book');
 const newBookBtn = document
-  .querySelector(".add-book-btn")
-  .addEventListener("click", () => (addBookForm.style.display = "flex"));
+  .querySelector('.add-book-btn')
+  .addEventListener('click', () => (addBookForm.style.display = 'flex'));
 
 // Close functionality new book form
 const closeBookForm = document
-  .querySelector(".close-form")
-  .addEventListener("click", () => (addBookForm.style.display = "none"));
+  .querySelector('.close-form')
+  .addEventListener('click', () => (addBookForm.style.display = 'none'));
 
 // Add button functionality new book form
 const addBookBtn = document
-  .querySelector(".add-form-btn")
-  .addEventListener("click", addBookToLibrary);
+  .querySelector('.add-form-btn')
+  .addEventListener('click', addBookToLibrary);
 
 // Clear button functionality new book form
 const clearFormBtn = document
-  .querySelector(".clear-form-btn")
-  .addEventListener("click", clearForm);
+  .querySelector('.clear-form-btn')
+  .addEventListener('click', clearForm);
 
 // Read/unread button functionality
 
@@ -39,14 +39,14 @@ class Book {
 }
 
 function clearForm() {
-  title.value = "";
-  author.value = "";
-  pages.value = "";
-  isRead.value = "Unread";
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  isRead.value = 'Unread';
 }
 
 function addBookToLibrary() {
-  addBookForm.style.display = "none";
+  addBookForm.style.display = 'none';
   addBook = new Book(title, author, pages, isRead);
   myLibrary.push(addBook);
   setData();
@@ -55,8 +55,8 @@ function addBookToLibrary() {
 }
 
 function render() {
-  const display = document.querySelector(".show-books");
-  const books = document.querySelectorAll(".card");
+  const display = document.querySelector('.show-books');
+  const books = document.querySelectorAll('.card');
 
   books.forEach((book) => display.removeChild(book));
 
@@ -66,7 +66,7 @@ function render() {
 }
 
 function createCard(book) {
-  const addCard = document.querySelector(".show-books");
+  const addCard = document.querySelector('.show-books');
 
   let id = myLibrary.indexOf(book);
 
@@ -81,27 +81,27 @@ function createCard(book) {
                   <button class="card-btn btn-del card-del-${id}">Delete</button>
                 </div>`;
 
-  addCard.insertAdjacentHTML("beforeend", card);
+  addCard.insertAdjacentHTML('beforeend', card);
 
   const changeRead = document.querySelector(`.card-read-${id}`);
 
-  if (book.isRead === "Read") {
-    changeRead.style.backgroundColor = "rgb(59, 182, 59)";
+  if (book.isRead === 'Read') {
+    changeRead.style.backgroundColor = 'rgb(59, 182, 59)';
   } else {
-    changeRead.style.backgroundColor = "rgb(217, 83, 79)";
+    changeRead.style.backgroundColor = 'rgb(217, 83, 79)';
   }
 
-  changeRead.addEventListener("click", () => {
+  changeRead.addEventListener('click', () => {
     console.log(`Working on button id ${id}`);
-    if (book.isRead === "Read") {
-      book.isRead = "Unread";
-      changeRead.textContent = "Unread";
-      changeRead.style.backgroundColor = "rgb(217, 83, 79)";
+    if (book.isRead === 'Read') {
+      book.isRead = 'Unread';
+      changeRead.textContent = 'Unread';
+      changeRead.style.backgroundColor = 'rgb(217, 83, 79)';
       setData();
     } else {
-      book.isRead = "Read";
-      changeRead.textContent = "Read";
-      changeRead.style.backgroundColor = "rgb(59, 182, 59)";
+      book.isRead = 'Read';
+      changeRead.textContent = 'Read';
+      changeRead.style.backgroundColor = 'rgb(59, 182, 59)';
       setData();
       console.log(book.isRead);
       console.log(myLibrary);
@@ -110,7 +110,7 @@ function createCard(book) {
 
   const delBook = document.querySelector(`.card-del-${id}`);
 
-  delBook.addEventListener("click", () => {
+  delBook.addEventListener('click', () => {
     myLibrary.splice(myLibrary.indexOf(book), 1);
     setData();
     render();
@@ -120,7 +120,7 @@ function createCard(book) {
 // Stores myLibrary array to localStorage
 
 function setData() {
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
 // Retrieves myLibrary array from localStorage
@@ -129,7 +129,7 @@ function getData() {
   if (!localStorage.myLibrary) {
     render();
   } else {
-    let library = JSON.parse(localStorage.getItem("myLibrary"));
+    let library = JSON.parse(localStorage.getItem('myLibrary'));
     myLibrary = library;
     render();
   }
